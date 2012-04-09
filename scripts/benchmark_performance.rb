ed23f4c288112b3880e531d290db8f09ee2329ee
George Nychis <gnychis@gmail.com>
2012-04-09 16:22:17 -0400

    quick checkin after generating the networks randomly

--- scripts/benchmark_performance.rb
+++ scripts/benchmark_performance.rb
@@ -1,17 +1,37 @@
 #!/usr/bin/ruby
 
 types=["802.11g","802.11n","ZigBee","AnalogPhone"]
-T=["802.11g" => 2000, "802.11n" => 2000, "ZigBee" => 2000, "AnalogPhone" => 2000]
-B=["802.11g" => 20000, "802.11n" => 20000, "ZigBee" => 5000, "AnalogPhone" => 1000]
+T={"802.11g" => 2000, "802.11n" => 2000, "ZigBee" => 2000, "AnalogPhone" => 2000}
+B={"802.11g" => 20000, "802.11n" => 20000, "ZigBee" => 5000, "AnalogPhone" => 1000}
 
-
+def airtime(type)
+  if(type=="802.11g" || type=="802.11n")
+    return rand
+  elsif(type=="ZigBee")
+    line=IO.readlines("exps.txt")
+    c = rand*line.length.to_i
+    return line[c-1].to_f
+  elsif(type=="AnalogPhone")
+    return 1
+  else
+    return -1
+  end
+end
 
 # Benchmark for 1 network, all the way up to 50 networks
-(1 .. 50).each do |n|
+(5 .. 5).each do |n|
 
+  fn=File.new("networks.dat","w")
+  
   # Just round robin the type of network used
   (1 .. n).each do |j|
-    
-  end
+    type = types[(j-1) % types.size];
 
+    # Generate the networks dat file
+    rat=airtime(type)
+    fn.puts "#{j} #{type} #{rat} #{B[type]} #{T[type]}"
+
+  end
+  
+  fn.close
 end
