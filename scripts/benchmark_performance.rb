0784a96efc5dddf6ee9313d8c34533d409a96ad4
George Nychis <gnychis@gmail.com>
2012-04-09 15:22:25 -0400

    adding in backup data files

--- scripts/benchmark_performance.rb
+++ scripts/benchmark_performance.rb
@@ -1,2 +1,17 @@
 #!/usr/bin/ruby
 
+types=["802.11g","802.11n","ZigBee","AnalogPhone"]
+T=["802.11g" => 2000, "802.11n" => 2000, "ZigBee" => 2000, "AnalogPhone" => 2000]
+B=["802.11g" => 20000, "802.11n" => 20000, "ZigBee" => 5000, "AnalogPhone" => 1000]
+
+
+
+# Benchmark for 1 network, all the way up to 50 networks
+(1 .. 50).each do |n|
+
+  # Just round robin the type of network used
+  (1 .. n).each do |j|
+    
+  end
+
+end
