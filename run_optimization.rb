c14444863dcce2ff1a973e54e458093953b1e2e1
George Nychis <gnychis@gmail.com>
2013-07-10 12:51:25 -0400

    keeping track of links and link ids now

--- run_optimization.rb
+++ run_optimization.rb
@@ -11,7 +11,8 @@ Trollop::die :directory, "must include map.txt" if(File.exist?("#{opts[:director
 Trollop::die :directory, "must include data in files labaled capture<#>.dat" if(Dir.glob("#{opts[:directory]}/capture*.dat").size<1)
 
 MapItem = Struct.new(:radioID, :protocol, :radioName, :netID, :frequencies)
-Link = Struct.new(:lID, :srcID, :dstID, :protocol, :freq, :rssi, :bandwidth, :airtime, :txLen, :backoff)
+Link = Struct.new(:lID, :srcID, :dstID, :protocol, :freq, :bandwidth, :airtime, :txLen)
+LinkView = Struct.new(:lID, :rssi, :backoff)
 
 def error(err)
   puts err
@@ -26,8 +27,8 @@ begin
   mapItemByID = Hash.new      # For keeping track of the device map, indexed by radioID
   mapItemByName = Hash.new    # Indexing it by radioName
 
-  linksByID = Hash.new    # Keep track the links that belong to specific node
-  linksByName = Hash.new  # Keep track of the links by name
+  linkViewsByID = Hash.new    # Keep track the links that belong to specific node
+  linkViewsByName = Hash.new  # Keep track of the links by name
 
   uridToRID = Array.new     # Keep track of unique ID (UID) for each ID
   ridToURID = Hash.new      # Go from ID to a UID
@@ -35,7 +36,8 @@ begin
   spatialRangeByID = Hash.new     # Keep track of all the radios that are within range of this node
   spatialRangeByName = Hash.new   # Keep track of the same information, but associated with a name
 
-  linkIDs = Hash.new
+  linkIDs = Hash.new        # Get a link ID by source and destination
+  links = Array.new         # Store the links, exactly at the index of the linkID
 
   #################################################################################################
   # Read in the map.txt file in to a data structure
@@ -64,6 +66,7 @@ begin
   # Now, go through each of the data files and read the link data associated to the node
   #######
   lastLinkID=0
+  links.push(nil)
   Dir.glob("#{opts[:directory]}/capture*.dat").each do |capfile|
     
     baselineRadio=nil           # Store the baseline radio for the capture file
@@ -80,8 +83,8 @@ begin
         baselineRadioInfo = mapItemByID[baselineRadio] if(baselineRadioInfo.nil?)
 
         # Now, just create some empty Arrays for this radio also
-        linksByID[baselineRadioInfo.radioID]=Array.new      if(linksByID[baselineRadioInfo.radioID].nil?)
-        linksByName[baselineRadioInfo.radioName]=Array.new  if(linksByName[baselineRadioInfo.radioName].nil?)
+        linkViewsByID[baselineRadioInfo.radioID]=Array.new      if(linkViewsByID[baselineRadioInfo.radioID].nil?)
+        linkViewsByName[baselineRadioInfo.radioName]=Array.new  if(linkViewsByName[baselineRadioInfo.radioName].nil?)
 
         spatialRangeByID[baselineRadioInfo.radioID]=Array.new       if(spatialRangeByID[baselineRadioInfo.radioID].nil?)    
         spatialRangeByName[baselineRadioInfo.radioName]=Array.new   if(spatialRangeByName[baselineRadioInfo.radioName].nil?)
@@ -97,8 +100,10 @@ begin
         lID = lastLinkID+1 
         linkIDs[[lSrc,lDst]]=lID
         lastLinkID+=1
+        pushLink=true
       else
         lID = linkIDs[[lSrc,lDst]]
+        pushLink=false
       end
 
       # Read in the link data
@@ -107,20 +112,25 @@ begin
                     ls[1],        # The destination ID for the link
                     ls[2],        # the protocol used for the link
                     ls[3].to_i,   # The frequency used
-                    ls[4].to_i,   # the RSSI from the transmitter to the baseline node
                     ls[5].to_i,   # The bandwidth used on the link
                     ls[6].to_f,   # The airtime observed on the link from the source to destination
-                    ls[7].to_i,   # The average transmission length in microseconds
+                    ls[7].to_i)   # The average transmission length in microseconds
+                    
+      lv = LinkView.new(lID,      # The link ID seen by this view
+                    ls[4].to_i,   # the RSSI from the transmitter to the baseline node
                     ls[8].to_i)   # Whether the baseline node backs off to this link
 
-      # Keep track of all links by their srcID, as we consider links belonging to the transmitter
-      linksByID[li[:srcID]]=Array.new if(not linksByID.has_key?(li[:srcID]))
-      linksByID[li[:srcID]].push( li )
+      # Store the link if we haven't seen it before
+      links.push(li) if(pushLink)
 
-      # Keep track of all links by their name also, if one exists.  This is for convenience.
+      # Keep track of all link views by their srcID, as we consider links belonging to the transmitter
+      linkViewsByID[li[:srcID]]=Array.new if(not linkViewsByID.has_key?(li[:srcID]))
+      linkViewsByID[li[:srcID]].push( lv )
+
+      # Keep track of all link views by their name also, if one exists.  This is for convenience.
       radioName = mapItemByID[li[:srcID]].radioName if(not mapItemByID[li[:srcID]].nil?)
-      linksByName[radioName]=Array.new if(not radioName.nil? and not linksByName.has_key?(radioName))
-      linksByName[radioName].push( li ) if(not radioName.nil?)
+      linkViewsByName[radioName]=Array.new if(not radioName.nil? and not linkViewsByName.has_key?(radioName))
+      linkViewsByName[radioName].push( lv ) if(not radioName.nil?)
 
       # Now keep track of all the radios within range of this baseline radio
       spatialRangeByID[baselineRadioInfo.radioID].push( li[:srcID] )
@@ -133,7 +143,7 @@ begin
   ## Now, we need a unique numeric ID for every single transmitter.  This is strictly for the
   ## MIP optimization representation.  We need to keep track of these and we can have a lookup.
   urid=1
-  linksByID.each_key do |rid|
+  linkViewsByID.each_key do |rid|
     ridToURID[rid]=urid
     uridToRID[urid]=rid
     urid+=1
@@ -153,7 +163,7 @@ begin
     # If there is no map item associated, then the possible set of frequencies is just the
     # frequency it is operating on.  We take this from any of the active links it is involved in.
     if(mi.nil?)
-      of.print "#{linksByID[rid][0].freq}e3}"
+      of.print "#{linkViewsByID[rid][0].freq}e3}"
     else
       mi[:frequencies].each_index do |i|
         of.print "#{mi[:frequencies][i]}e3"
@@ -195,7 +205,7 @@ begin
   ## Now we go through and prepare the links and transfer them over to the optimization.  We first
   ## need to condense the links so that there is only a single "link" for every transmitter and
   ## receiver.
-  puts linksByID.inspect
+  #puts linkViewsByID.inspect
   
   #################################################################################################
   ## Go through all of the radios and place them in to coordination or not
