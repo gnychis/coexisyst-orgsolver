e76a71798fcd27ca0d1e4570cf2d5a3f40546839
George Nychis <gnychis@gmail.com>
2013-07-25 01:53:32 -0400

    running the first test now

--- test_suite.rb
+++ test_suite.rb
@@ -12,7 +12,7 @@ begin
   hgraph=Hypergraph.new
 
   # Create 6 radios that have independent links
-  (1..6).each {|rid| hgraph.newRadio( Radio.new(rid, "802.11agn", "wifi#{rid}", "network#{(rid-1)/2}", [2412,2436,2462])) }
+  (1..6).each {|rid| hgraph.newRadio( Radio.new("#{rid}", "802.11agn", "wifi#{rid}", "network#{(rid-1)/2}", [2412,2436,2462])) }
 
   # Create links between the pairs of radios
   hgraph.newLinkEdge( LinkEdge.new( "1","2", 2436, 20, 0.4, 0.75, 2750, "802.11agn") )
@@ -25,7 +25,8 @@ begin
       next if(le1==le2)
       hgraph.newSpatialEdge( SpatialEdge.new(le1.srcID,le2.srcID,-40,1) ) } }
 
-
+  opt = Optimization.new(hgraph)
+  opt.run.each {|i| puts "#{i[0].radioID} #{i[0].networkID} #{i[1]}"  }
 
 end
 
