0a762ec5f002331e26f8a7870ad7ccee4e3f2dda
George Nychis <gnychis@gmail.com>
2013-07-25 15:25:27 -0400

    need to figure out issue with asymmetric scenario

--- test_suite.rb
+++ test_suite.rb
@@ -344,5 +344,33 @@ begin
   Optimization.new(hgraph).run
   intermed_test("should avoid channel 2437")
   (hgraph.getRadio("5").activeFreq==2437) ? test_result(false) : test_result(true)
-  
 end
+
+#begin
+#  new_intermed_test("Testing asymmetric weights")
+#  hgraph=nil
+#  hgraph=Hypergraph.new
+#
+#  # Create 4 radios that have independent links
+#  (1..2).each {|rid| hgraph.newRadio( Radio.new("#{rid}", "802.11agn", "wifi#{rid}", "network#{(rid-1)/2}", [2412])) }
+#  (3..4).each {|rid| hgraph.newRadio( Radio.new("#{rid}", "802.11agn", "wifi#{rid}", "network#{(rid-1)/2}", [2437])) }
+#  (5..6).each {|rid| hgraph.newRadio( Radio.new("#{rid}", "802.11agn", "wifi#{rid}", "network#{(rid-1)/2}", [2412,2437])) }
+#
+#  # Create links between the pairs of radios
+#  hgraph.newLinkEdge( LinkEdge.new( "1","2", 2437, 20, 0.4, 0.5, 3750, "802.11agn") )
+#  hgraph.newLinkEdge( LinkEdge.new( "3","4", 2437, 20, 0.4, 0.5, 2750, "802.11agn") )
+#  hgraph.newLinkEdge( LinkEdge.new( "5","6", 2437, 20, 0.2, 0.3, 2750, "802.11agn") )
+#  hgraph.newSpatialEdge( SpatialEdge.new("5","6",-40,0) )
+#
+#  # First, the two transmitters are within range of the receiver
+#  hgraph.newSpatialEdge( SpatialEdge.new("1","6",-20,0) )
+#  hgraph.newSpatialEdge( SpatialEdge.new("3","6",-20,0) )
+#
+#  # Now, create the spatial edges so that it is completely hidden from 1
+#  hgraph.newSpatialEdge( SpatialEdge.new("5","1",-20,1) )
+#  hgraph.newSpatialEdge( SpatialEdge.new("5","3",-20,1) )
+#
+#  Optimization.new(hgraph).run
+#  intermed_test("should avoid channel 2412")
+#  (hgraph.getRadio("5").activeFreq==2412) ? test_result(false) : test_result(true)
+#end
